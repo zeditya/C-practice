bdf27cab7b248f699d6fd064d82e022dc092d09e
zeditya <adityabahuguna098@gmail.com>
2025-08-31 18:44:17 +0530

    escape sequences

---
+++ 2 variables and datatypes/escapesequences.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+
+  printf("learning\nc\n");
+  printf("tabular\tsequence\n");
+  printf("double\"quotes\"\n");
+  printf("single\'quote\'\n");
+  printf("backspace\bsequence\n");
+  printf("double\\s");
+
+}
