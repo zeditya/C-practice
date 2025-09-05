c109c200711c5986b8fc25b3b4d12ea90f4d768e
zeditya <adityabahuguna098@gmail.com>
2025-09-05 20:15:40 +0530

    sum of two numbers

---
+++ 2 variables and datatypes/sumof2num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+  int num1 , num2 , sum ;
+  printf("welcome to the sum calculator\n");
+  printf("enter the first number\n");
+  scanf("%d", &num1);
+  printf("enter number 2\n");
+  scanf("%d", &num2);
+
+  sum = num1 + num2;
+  printf("sum of the two numbers is %d");
+  return 0;
+
+
+
+
+}
